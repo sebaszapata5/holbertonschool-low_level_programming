701f57905845e0e00cfd83e806460e87bc504006
sebaszapata5 <1640@holbertonschool.com>
2020-02-05 22:39:09 +0000

    c++

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Variables
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Variables
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int entero;
