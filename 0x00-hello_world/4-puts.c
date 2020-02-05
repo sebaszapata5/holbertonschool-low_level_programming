8106516936ee118b37a8f7eac6ae5d27f1320e49
sebaszapata5 <1640@holbertonschool.com>
2020-02-05 21:29:34 +0000

    program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,2 @@
 #include <studio.h>
-/**
- * main - Entry point
- *
- *Return: Always 0 (Succes)
- */
-int main(void)
-{ 
-  puts ("\"Programming is like building a multilingual puzzle");
-  return (0);
-}
+int main(void) | puts ("\"Programming is like building a multilingual puzzle"); | return (0);
