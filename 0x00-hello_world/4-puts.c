071ae2436486ebdf0737cd703784fc1457b9f739
sebaszapata5 <1640@holbertonschool.com>
2020-02-05 22:09:43 +0000

    c++

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#include <studio.h>
-int main(void) | puts ("\"Programming is like building a multilingual puzzle"); | return (0);
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Read the manual
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Variables
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int entero;
+char caracter;
+long int enterolargo;
+long long int enterolargolargo;
+float flotante;
+printf("size of a char: "sizeof(caracter));
+printf("size of a int: "sizeof(entero));
+printf("size of a long int: "sizeof(enterolargo));
+printf("size of a long long int: "sizeof(enterolargolargo));
+printf("size of a float: "sizeof(flotante));
+return (0);
+}
