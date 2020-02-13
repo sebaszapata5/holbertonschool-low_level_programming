3f28b9a2f4d84fbbcf2a2bd2c5a2235b3d929cf5
sebaszapata5 <1640@holbertonschool.com>
2020-02-13 22:20:03 +0000

    more_functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,24 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
-	{
-		printf("%s ", "FizzBuzz");
-	}
+		{
+			printf("%s", "FizzBuzz");
+		}
 	else
 	{
 		if ((i % 3) == 0)
 		{
-			printf("%s ", "Fizz");
+			printf("%s", "Fizz");
 		}
 		else
 		{
 			if ((i % 5) == 0)
 		{
-			printf("%s ", "Buzz");
+			printf("%s", "Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 		}
 	}
