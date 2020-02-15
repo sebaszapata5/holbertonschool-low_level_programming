ed4e73ec33f5c45dd02d3fe26dced07f9ae04cef
sebaszapata5 <1640@holbertonschool.com>
2020-02-15 01:59:24 +0000

    more_functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,16 @@
  */
 void print_number(int n)
 {
-        unsigned int m = n;
+	unsigned int m = n;
 
-                if (n < 0)
-                {
-			_putchar('-');
-			m = -m;
-		}
-		if ((m / 10) > 0)
-		{
-			print_number (m / 10);
-		}
-		_putchar((m % 10) + '0');
-
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -m;
+	}
+	if ((m / 10) > 0)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
 }
