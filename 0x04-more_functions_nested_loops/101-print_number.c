597ef59262c5f8d8cdadb428460f543d906e58e2
sebaszapata5 <1640@holbertonschool.com>
2020-02-15 01:55:31 +0000

    more_functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,30 +1,22 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_number - check the code for Holberton School students.
- * @n: _putchar
+ * @n: VARIABLE
  * Return: Always 0.
  */
 void print_number(int n)
 {
-        int tam;
-        int lon;
+        unsigned int m = n;
 
-        tam = 1;
-        while (tam <= 100000000)
-        {
-                if ((n / tam) != 0)
+                if (n < 0)
                 {
-                        lon = tam;
-                }
-                tam = (tam * 10);
-                printf ("%d \n" , lon);
-        }
-        while (lon >= 10)
-        {
-                _putchar('0' + (n / lon));
-                lon = (lon / 10);
-        }
-        _putchar('0' + (n % 10));
+			_putchar('-');
+			m = -m;
+		}
+		if ((m / 10) > 0)
+		{
+			print_number (m / 10);
+		}
+		_putchar((m % 10) + '0');
 
 }
