b25df4b32d7923d5f02eb0533e6e59aa3e721b65
sebaszapata5 <1640@holbertonschool.com>
2020-02-18 18:28:43 +0000

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,10 +16,10 @@ void puts_half(char *str)
 	{
 		if ((length_of_the_string % 2) != 0)
 			{
-				n = (length_of_the_string - 1) / 2;
+				n = (length_of_the_string - 2) / 2;
 				n++;
 			}
-	length_of_the_string++;
+		length_of_the_string++;
 	}
 	n++;
 	while (str[n] != '\0')
